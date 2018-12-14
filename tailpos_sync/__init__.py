e49704df614b40c2dea1e51e96decf5e7db6e198
Ivan Ray Altomera <irayspacii@gmail.com>
2018-12-14 18:11:20 +0800

    Initial syncing function

--- tailpos_sync/__init__.py
+++ tailpos_sync/__init__.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
+import frappe
 
 __version__ = '0.0.4'
 
@@ -8,3 +9,16 @@ __version__ = '0.0.4'
 # - Created 'Receipts Closing Voucher' for creation of Sales Invoice
 # =========
 
+
+@frappe.whitelist()
+def sync():
+    hash = ''
+
+    item_hash = frappe.db.get_single_value('Revision Hashes', 'item_hash')
+
+    if hash == item_hash:
+        return {'uptodate': 1}
+    else:
+        return {
+            'items': frappe.get_all('Item', fields=['name', 'standard_rate'])
+        }
